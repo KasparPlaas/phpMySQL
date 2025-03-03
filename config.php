5108be88dd7c3f89c341b3a12b11fbdedeff4231
KasparPlaas <kaspar.plaas@hkhk.edu.ee>
2025-03-03 10:27:39 +0200

    asdasd

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+//***protseduuriline***//
+//sinu andmed
+$db_server = 'localhost';
+$db_andmebaas = 'muusikapood';
+$db_kasutaja = 'kaspar';
+$db_salasona = 'Passw0rd';
+//ühendus andmebaasiga
+$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+//ühenduse kontroll
+if(!$yhendus){
+	die('Ei saa ühendust andmebaasiga');
+}
+?>
